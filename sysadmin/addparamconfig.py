c12175dad9188909a5938950d7d06efd125ed952
sunlnx <sunlnx@gmail.com>
2017-06-10 20:07:05 +0530

    added config parameter to apache config file

--- sysadmin/addparamconfig.py
+++ sysadmin/addparamconfig.py
@@ -3,41 +3,51 @@
 
 # if the entry already in the config file it would just exit
 
-configfile = '/tmp/smb.conf'
-param = 'name=value'
+configfile='/etc/httpd/conf/httpd.conf'
+param ='ServerName www.fedora.com:80'
 
-def verifyParameter():
-    try:
-        with open(configfile,'r') as fp:
-                if param in fp.read():
-                    print("Info:'{}' already exists in '{}'".format(param, configfile))
-                    return 1
-                else:
-                    return 0
-    except FileNotFoundError as err:
-        print(err)
-        return 0
+def skipComments(file):
+    """generator which would skip all blank lines and '#' char"""
+    for line in file:
+        if not line.strip().startswith('#'):
+            yield line
 
-def writeParameter():
-        with open(configfile,'a') as fp:
-            fp.write("\n"+param)
-            print("Info:'{}' added to {}'".format(param,configfile))
-            return 1
+def paramExists():
+    """check if the defined parameter exists from non-commented files in the config file"""
+    with open(configfile,'r') as fp:
+        for line in skipComments(fp):
+            paramcount=line.count(param)
+
+        if paramcount != 0:
+            return True
+        else:
+            return False
+
+def changeConfigFile():
+    """
+    On successful return from the paramExists required to write change to the 
+    config file """
+
+    with open(configfile,'a') as fp:
+        fp.write("\n"+param)
+        print("Info:'{}' added to {}'".format(param,configfile))
 
 def restartService():
+    """ restart service once your config file changed"""
     import subprocess
     output = subprocess.call(["sudo systemctl restart httpd.service"],shell=True)
     if output == 0:
-        print('service restart successful')
+        print('Info: service restart successful')
     else:
-        print('service restart failed')
+        print('Info: service restart failed')
 
 if __name__ == '__main__':
-    if verifyParameter():
-        # entry already in config file.. exting from prog
-        exit(0)
-        # check file exists if so append entry
-    elif writeParameter():
-        restartService()
+
+    retval = paramExists()
+    if retval == True:
+        print("Info:'{}' already exists in '{}'".format(param, configfile))
+    else:
+        changeConfigFile()
+    restartService()
 
 
