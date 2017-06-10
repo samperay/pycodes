2bb5058cee03c60bef31e65a517fe49fac2fecf8
sunlnx <sunlnx@gmail.com>
2017-06-10 14:43:07 +0530

    module for restating service

--- sysadmin/addparamconfig.py
+++ sysadmin/addparamconfig.py
@@ -13,8 +13,7 @@ def verifyParameter():
                     print("Info:'{}' already exists in '{}'".format(param, configfile))
                     return 1
                 else:
-                    return 0
-    except FileNotFoundError as err:
+                    return 0    except FileNotFoundError as err:
         print(err)
         return 0
 
@@ -24,13 +23,13 @@ def writeParameter():
             print("Info:'{}' added to {}'".format(param,configfile))
             return 1
 
-# def restartService():
-#     import subprocess
-#     output = subprocess.call(["sudo systemctl restart httpd.service"],shell=True)
-#     if output == 0:
-#         print('service restart successful')
-#     else:
-#         print('service restart failed')
+def restartService():
+    import subprocess
+    output = subprocess.call(["sudo systemctl restart httpd.service"],shell=True)
+    if output == 0:
+        print('service restart successful')
+    else:
+        print('service restart failed')
 
 if __name__ == '__main__':
     if verifyParameter():
@@ -38,7 +37,6 @@ if __name__ == '__main__':
         exit(0)
         # check file exists if so append entry
     elif writeParameter():
-        print("True")
-        #restartService()
+        srestartService()
 
 
