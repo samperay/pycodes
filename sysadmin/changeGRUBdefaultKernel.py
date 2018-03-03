edc83cc23eb3fca7598abcff565968a48d1da66b
sunilka <sunilka@gmail.com>
2018-03-03 18:19:18 +0530

    Displays current default GRUB entry and one can select their choice of kernels which will boot over next reboot.    COMPLETED

--- sysadmin/changeGRUBdefaultKernel.py
+++ sysadmin/changeGRUBdefaultKernel.py
@@ -1,13 +1,31 @@
 # lists the kernel versions and enter to boot to your default kernels
 
+def findDefaultGrubLevel():
+    """Find out default grub entry"""
+    with open('/boot/grub/grub.conf', 'r') as file:
+        # filedata = file.read().replace('default=2','default='+str(choice))
+        for line in file.readlines():
+            if 'default' in line:
+                currentdefaultgurb = line.split('=')[1]
+                return  currentdefaultgurb
 
-def changeGrubDefaultConfig():
+def changeGrubDefaultConfig(choice):
     """Pass the entry to change default booting config for the grub"""
+    filedata = None
 
+    defaultgrubentry=findDefaultGrubLevel()
+    srcstring='default='+defaultgrubentry
+    deststring='default='+choice+"\n"
+
+    with open('/boot/grub/grub.conf','r') as file:
+        filedata = file.read().replace(srcstring,deststring)
+
+    with open('/boot/grub/grub.conf','w') as file1:
+        file1.write(filedata)
 
 def displayKernelsInstalled():
     """Print default kernels installed in system"""
-
+    defaultkernelslist=[]
     count = 0
     with open('/boot/grub/grub.conf') as f:
         print("----" * 20)
@@ -18,19 +36,23 @@ def displayKernelsInstalled():
                 print(count,"\t\t|", line.strip())
                 count+=1
         print("----" * 20)
-        print(count)
+        #defaultkernelslist.append(count)
+
+        for i in range(count):
+            defaultkernelslist.append(i)
 
     choice = input('Enter your choice to boot kernel by default next time: ')
     if (int(choice) <= count):
-        print('Choice entered', choice)
+        print('Choice entered',choice)
+        if int(choice) in defaultkernelslist:
+            changeGrubDefaultConfig(choice)
     else:
-        print('No such value as that')
-
-    if count in int(choice):
-        changeGrubDefaultConfig()
+        print('No such grub entry found')
 
 if __name__ == '__main__':
+    print('Current your GRUB default entry:', findDefaultGrubLevel())
     displayKernelsInstalled()
+
 
 
 
