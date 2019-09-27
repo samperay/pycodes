6b9297d1aebbea0e82104f2a3e5eb256e393683d
samperay <sunlnx@gmail.com>
2019-09-27 14:52:00 +0530

    code added with mutability

--- py-refresh/py-Basic.py
+++ py-refresh/py-Basic.py
@@ -181,3 +181,44 @@
 
 # print(name, profession)  # Bob Mechanic
 
+##### ============== Mutability ================= #####
+
+
+# a = []
+# b = a
+
+# # Remember a and b are _names_ for the list. They both have the _same_ value.
+# a.append(35)  # Modify the value.
+
+# print(a)
+# print(b)
+
+# # Here they are different lists, because [] creates a new list every time. You can check whether two things are the _same_ one by usingt the `id()` function:
+
+# print(id(a))
+# print(id(b))  # Different from id(a)
+
+# -- immutable --
+
+# Some values can't be changed because they don't have methods that modify the value itself.
+# In case of the list, `.append()` mutates the list.
+# For example integers don't have any such methods, so they are called _immutable_.
+
+# a = 8597
+# b = 8597
+
+# print(id(a))
+# print(id(b))  # Same one
+
+# a = 8598
+
+# print(id(a))
+# print(
+#     id(b)
+# )  # Different, because we didn't change 8597. We just used the name 'a' for a different value. 'b' still is a name for 8597.
+
+# # Most things are mutable in Python. If you want to keep one of your classes immutable, don't add any methods that change the objects' properties.
+
+# # Tuples and strings are the only fundamental collection in Python which is immutable.
+# # Lists, sets, dictionaries are all mutable.
+# # Integers, floats, and booleans are all immutable.
