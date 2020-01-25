637fa66fe306f0cc1adc2e6bb9da90dbb85df431
samperay <sunlnx@gmail.com>
2020-01-25 11:15:35 +0530

    eg on chap10 completed

---
+++ py-refresh/think-python-examples/chapter10/remove_duplicate.py
@@ -0,0 +1,2 @@
+# Exercise 10.6 Write a function called remove_duplicates that takes a list and returns a new list with only the unique elements from the original. Hint: they don’t have to be in the same order.
+
