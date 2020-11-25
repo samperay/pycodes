6405b2d48a4824785a1e9b88fb290c16210e90e9
samperay <sunlnx@gmail.com>
2020-11-25 06:54:20 +0000

    basic level code added

---
+++ examples/basic/merge_lists.py
@@ -0,0 +1,8 @@
+# Merge two lists 
+
+a = [3, 4, 6, 10, 11, 18]
+b = [1, 5, 7, 12, 13, 19, 21]
+
+a.extend(b)
+
+print("asending list sort:",sorted(a))
--- examples/largest.py
+++
@@ -1,3 +0,0 @@
-num = [1,20,40,23,12]
-l_index=len(num)-1
-print("Largest:",sorted(num)[l_index])
