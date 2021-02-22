1b7ecdae5835f34b758ea835bb19bd29bb430c71
samperay <sunlnx@gmail.com>
2021-02-22 08:35:10 +0530

    fibgen

--- examples/intermediate/fibonacci.py
+++ examples/intermediate/fibonacci.py
@@ -20,6 +20,16 @@ for i in xrange(0,10):
     print(a)
     a,b=b,a+b
 
+# Method 4 - Generator
+def fibgen(n):
+    a,b=0,1
+    for i in xrange(0,num):
+        yield(i+1,a)
+        a,b=b,a+b
+
+for item in fibgen(10):
+    print(item)
+
 # main func
 for i in range(10):
     print("Fib #1:",fib1(i))
