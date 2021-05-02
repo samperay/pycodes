e59f20c151b2d974b0854416aad1407af565d2cc
samperay <sunlnx@gmail.com>
2021-05-02 10:13:59 +0530

    dict of even squares

--- examples/basic/dict_create.py
+++ examples/basic/dict_create.py
@@ -4,3 +4,9 @@ print("Method-1",d)
 
 d1={k:k**2 for k in range(10)}
 print("Method-2",d1)
+
+# create dictionamry of even squares
+even={k:k**2 for k in range(10) if k%2==0}
+print("Dict of Even Squares: ",even)
+
+# you can create similar list of cube squares.
