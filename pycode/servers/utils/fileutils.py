398ce04b7e2d28a108fd7af5d77db1fc3e79996b
samperay <sunlnx@gmail.com>
2021-05-13 22:01:49 +0530

    code for fileutils added

---
+++ pycode/servers/utils/fileutils.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+
+import os
+class FileOps:
+    """
+    file operations:
+
+    listfiles: list files in the path specified
+    listdirectory: list all the directories in path specified
+    """
+    # Defaults to users home directoty
+    def __init__(self):
+        path = os.path.expanduser("~")
+        self.path = path
+
+    def listfiles(self,path):
+        self.path = path
+        listofallfiles = []
+        for _,_,listoffiles in os.walk(self.path):
+            for eachfile in listoffiles:
+                listofallfiles.append(eachfile)
+        return listofallfiles
+
+    def listdirectory(self,path):
+        self.path = path
+        listofalldirs = []
+        for _,listofdirs,_ in os.walk(self.path):
+            for eachdirectory in listofdirs:
+                if eachdirectory.startswith("."):
+                    continue
+                else:
+                    listofalldirs.append(eachdirectory)
+        return listofalldirs
+
+# # Only used for testing !
+#
+# files = FileOps()
+# for eachitem in files.listfiles("/etc"):
+#     print(eachitem)
