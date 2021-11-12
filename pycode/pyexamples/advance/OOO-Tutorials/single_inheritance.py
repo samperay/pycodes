9b78b4bfd28c60fc7c7a1e1f3eb6e062b8d2ca5b
samperay <sunlnx@gmail.com>
2021-11-12 12:50:35 +0530

    example for single inheritance

--- pycode/pyexamples/advance/OOO-Tutorials/single_inheritance.py
+++ pycode/pyexamples/advance/OOO-Tutorials/single_inheritance.py
@@ -7,13 +7,24 @@ class Apple:
 
 
 class MacBook(Apple):
+
+    # protected attribute, you would just say that use this attribute only in this class and no where else
+
+    _color = "Grey"
+
+    # private attribute
+    __password = "private"  # NameDangling i.e _Apple__companyManufactureYear
+
     def __init__(self):
         self.yearOfManufacturer = 2021
 
     def manufacturerDetails(self):
-        print("Macbook manufactured in {} by {}".format(self.yearOfManufacturer, self.manufacturer))
+        print("Macbook manufactured in {} by {}".format(
+            self.yearOfManufacturer, self.manufacturer))
 
 
 macbook = MacBook()
 macbook.manufacturerDetails()
 macbook.contactDetails()
+print("Protected Attribute: ", macbook._color)
+print("Private Attribute: ", macbook._MacBook__password)
